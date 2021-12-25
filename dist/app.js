598bd0bf9d868b34e1dcf6a0a5991bf58b4b3802
javierpb275 <javierpb275@gmail.com>
2021-12-25 20:13:06 +0100

    created get route in authRoutes and added authRoutes to the app.

--- dist/app.js
+++ dist/app.js
@@ -4,8 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const auth_1 = __importDefault(require("./routes/auth"));
 const app = (0, express_1.default)();
 //settings
 app.set("port", process.env.PORT || 3000);
+//routes
+app.use(auth_1.default);
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Application } from 'express';
+import authRoutes from './routes/auth';
 
 const app: Application = express();
 
 //settings
 app.set("port", process.env.PORT || 3000);
+
+//routes
+app.use(authRoutes);
 
 export default app;
