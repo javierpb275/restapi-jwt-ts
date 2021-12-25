50c9fad6f8898c27b61f65347c44bd0ef0edabf5
javierpb275 <javierpb275@gmail.com>
2021-12-25 20:20:57 +0100

    installed morgan an added middleware morgan

--- dist/app.js
+++ dist/app.js
@@ -5,9 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const auth_1 = __importDefault(require("./routes/auth"));
+const morgan_1 = __importDefault(require("morgan"));
 const app = (0, express_1.default)();
 //settings
 app.set("port", process.env.PORT || 3000);
+//middlewares
+app.use((0, morgan_1.default)('dev'));
 //routes
 app.use(auth_1.default);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application } from 'express';
 import authRoutes from './routes/auth';
+import morgan from 'morgan';
 
 const app: Application = express();
 
 //settings
 app.set("port", process.env.PORT || 3000);
+
+//middlewares
+app.use(morgan('dev'));
 
 //routes
 app.use(authRoutes);
